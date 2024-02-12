a42c1fc50bdc5cdfb7abb1429e0e515fa2ca0d76
GeorgeOprea <oprea.george14@yahoo.com>
2024-02-12 21:29:01 +0200

    finished Hero component

--- next-destination/components/Hero.tsx
+++ next-destination/components/Hero.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { FaCirclePlay } from "react-icons/fa6";
+import { IoClose } from "react-icons/io5";
 
 const Hero = () => {
   return (
@@ -49,9 +50,31 @@ const Hero = () => {
             type="button"
             className="flex justify-center items-center gap-3 rounded-full border-white bg-white px-8 py-3 "
           >
-            <FaCirclePlay className=" text-green-500 " />
+            <FaCirclePlay className=" text-gray-500 " />
             <span className="font-bold">How does it work?</span>
           </button>
+        </div>
+      </div>
+
+      <div className="relative flex flex-1 items-start">
+        <div className="z-20 flex w-[268px] flex-col gap-8 rounded-3xl bg-gray-500 px-7 py-8">
+          <div className="flex flex-col">
+            <div className="flex items-center justify-between">
+              <p className="text-[16px] font-[400] text-gray-20">Location</p>{" "}
+              <IoClose width={24} height={24} />
+            </div>
+            <p className="text-[20px] font-[700] text-white">Montana Vista</p>
+          </div>
+          <div className="flex items-center justify-between">
+            <div className="flex flex-col">
+              <p className="text-[16px] font-[400] text-gray-20">Distance</p>
+              <p className="text-[20px] font-[700] text-white">134km</p>
+            </div>
+            <div className="flex flex-col">
+              <p className="text-[16px] font-[400] text-gray-20">Elevation</p>
+              <p className="text-[20px] font-[700] text-white">1.2km</p>
+            </div>
+          </div>
         </div>
       </div>
     </section>
