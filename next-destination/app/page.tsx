6a42e45068fcf08c3e4b3e5ac188ee48b55b98cc
GeorgeOprea <oprea.george14@yahoo.com>
2024-02-13 19:56:27 +0200

    started the Guide component

--- next-destination/app/page.tsx
+++ next-destination/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/Hero";
 import Camp from "@/components/Camp";
+import Guide from "@/components/Guide";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Camp />
+      <Guide />
     </>
   );
 }
---
+++ next-destination/components/Guide.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Guide = () => {
+  return (
+    <section className="flex flex-col justify-center items-center"></section>
+  );
+};
+
+export default Guide;
