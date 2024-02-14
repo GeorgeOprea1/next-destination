fa8b276d56ac4869cf197ddd10ca2f80f2479196
GeorgeOprea <oprea.george14@yahoo.com>
2024-02-14 20:33:54 +0200

    started the Features component

--- next-destination/app/page.tsx
+++ next-destination/app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "@/components/Hero";
 import Camp from "@/components/Camp";
 import Guide from "@/components/Guide";
+import Features from "@/components/Features";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <Camp />
       <Guide />
+      <Features />
     </>
   );
 }
